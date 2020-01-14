4a27ac2bee44aabc80b66659e983f93c15fd91b1
EXIST <18582414+EXYZED@users.noreply.github.com>
2020-01-14 00:04:13 +0000

    General Updates

--- Scripts/FrontPageAnimations.js
+++ Scripts/FrontPageAnimations.js
@@ -11,19 +11,20 @@ function OutFMTransition(str) {
     if (str == 'Broadcasting') {
       var el = document.querySelector('body');
       var newEl = document.createElement('p');
-      newEl.innerHTML = '<b>	Error 403 Authentication Required</b>';
+      newEl.innerHTML = '<b>	403 - Group Authentication required.</b>';
       el.parentNode.replaceChild(newEl, el);
     }
-	if (str == 'Broadcasting') {
+	if (str == 'Products') {
       var el2 = document.querySelector('body');
       var newEl2 = document.createElement('p');
-      newEl2.innerHTML = '<b>	Error 403 Authentication Required</b>';
+      newEl2.innerHTML = '<b>	403 - Group Authentication required.</b>';
       el2.parentNode.replaceChild(newEl2, el2);
     }
-	if (str == 'Broadcasting') {
+	if (str == 'Group') {
+	  window.location.href = 'https://AsfaltoAscari.com/Group.html';
       var el3 = document.querySelector('body');
       var newEl3 = document.createElement('p');
-      newEl3.innerHTML = '<b>	Error 403 Authentication Required</b>';
+      newEl3.innerHTML = '<b>	403 - Group Authentication required.</b>';
       el3.parentNode.replaceChild(newEl3, el3);
     }
 	if (str == 'Asfalto') {
